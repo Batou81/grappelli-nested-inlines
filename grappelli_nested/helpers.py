4acd717caa33704cd815032712d2afa9886c3f38
Manuel Dominguez <batou81@gmail.com>
2014-09-09 15:11:22 +0200

    Fix conflict on django 1.7.

--- grappelli_nested/helpers.py
+++ grappelli_nested/helpers.py
@@ -1,25 +1,28 @@
-from django.contrib.admin.helpers import AdminErrorList, InlineAdminFormSet
+from django.contrib.admin.helpers import InlineAdminFormSet
+from django import forms
 from django.utils import six
 
-class AdminErrorList(AdminErrorList):
+class AdminErrorList(forms.utils.ErrorList):
     """
     Stores all errors for the form/formsets in an add/change stage view.
     """
     def __init__(self, form, inline_formsets):
+        super(AdminErrorList, self).__init__()
+
         if form.is_bound:
-            self.extend(form.errors.values())
+            self.extend(list(six.itervalues(form.errors)))
             for inline_formset in inline_formsets:
                 self._add_formset_recursive(inline_formset)
-                    
+
     def _add_formset_recursive(self, formset):
         #check if it is a wrapped formset
         if isinstance(formset, InlineAdminFormSet):
             formset = formset.formset
-            
+
         self.extend(formset.non_form_errors())
         for errors_in_inline_form in formset.errors:
             self.extend(list(six.itervalues(errors_in_inline_form)))
-        
+
         #support for nested formsets
         for form in formset:
             if hasattr(form, 'nested_formsets'):
